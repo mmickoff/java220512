c72de0ff261b489986afc4ebb4071defef4b0962
mmickoff <mickmail2000@gmail.com>
2022-05-22 02:02:32 +0300

    220522-03-java220512-lesson3-hw3-8pp

---
+++ src/main/java/lesson3/Lesson3_0.java
@@ -0,0 +1,14 @@
+package lesson3;
+
+public class Lesson3_0 {
+
+    public static void main(String[] args) {
+        multiply(3, 4);
+    }
+
+    private static void multiply(int a, int b) {
+        int multiply = a * b;
+        System.out.printf ("Произведение %d и %d равно %d%n", a, b, multiply);
+
+    }
+}
