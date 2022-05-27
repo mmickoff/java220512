7a7d4c1102cc85339d2a3cc9c2519458bb02f17d
mickmail2000@gmail.com <mickmail2000@gmail.com>
2022-05-27 05:47:30 +0300

    220526-java220512-01-Lesson4.HW4-Maim-XO

--- src/main/java/lesson4/Main.java
+++ src/main/java/lesson4/Main.java
@@ -1,7 +1,5 @@
 package lesson4;
 
-import org.w3c.dom.ls.LSOutput;
-
 import java.util.Random;
 import java.util.Scanner;
 
@@ -21,16 +19,12 @@ public class Main {
     private static Scanner in = new Scanner(System.in);
     private static Random random = new Random();
 
+    private static int turnsCount = 0;
 
     public static void main(String[] args) {
         initMap();
         printMap();
         playGame();
-    }
-
-    private static void printMap() {
-        printMapHeader();
-        printMapBody();
     }
 
     private static void initMap() {
@@ -41,10 +35,15 @@ public class Main {
         }
     }
 
+    private static void printMap() {
+        printMapHeader();
+        printMapBody();
+    }
+
     private static void printMapHeader() {
         System.out.print(HEADER_FIRST_SYMBOL + SPACE_MAP);
         for (int i = 0; i < SIZE; i++) {
-            System.out.print(/*SPACE_MAP + */i + 1 + SPACE_MAP);
+            System.out.print(i + 1 + SPACE_MAP);
         }
         System.out.println();
     }
@@ -63,11 +62,16 @@ public class Main {
         while (true) {
             humanTurn();
             printMap();
-//        конец игры;
+            if (checkEnd(DOT_HUMAN)) {
+                break;
+            }
 
             aiTurn();
             printMap();
-//        конец игры;
+            if (checkEnd(DOT_AI)) {
+                break;
+            }
+
         }
 
 
@@ -79,18 +83,106 @@ public class Main {
         int rowNumber;
         int columnNumber;
 
-        System.out.println("Введите номер строки: "); 
-        rowNumber = in.nextInt() - 1;
-        
-        System.out.println("Введите номер столбца: "); 
-        columnNumber = in.nextInt() - 1;
+        while (true) {
+            System.out.println("Введите номер строки: ");
+            rowNumber = in.nextInt() - 1;
 
+            System.out.println("Введите номер столбца: ");
+            columnNumber = in.nextInt() - 1;
+
+            if (isCellFree(rowNumber, columnNumber)) {
+                break;
+            } else {
+                System.out.printf("Ячейка с координатами: %d:%d занята. Переходите. ", rowNumber + 1, columnNumber + 1);
+            }
+        }
         MAP[rowNumber][columnNumber] = DOT_HUMAN;
+        turnsCount++;
     }
 
+    private static boolean checkEnd(char symbol) {
+
+        if (checkWin(symbol)) {
+            if (symbol == DOT_HUMAN) {
+                System.out.println(("Ура! Вы победили!"));
+            } else {
+                System.out.println("Восстание машин близко - победил ИИ...");
+            }
+            return true;
+        }
+        if (checkDraw()) {
+            System.out.println("Увы.... никто не победил. Попробуйте ещё раз... ");
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean checkWin(char symbol) {
+        if (MAP[0][0] == symbol && MAP[0][1] == symbol && MAP[0][2] == symbol) {
+            return true;
+        }
+        if (MAP[1][0] == symbol && MAP[1][1] == symbol && MAP[1][2] == symbol) {
+            return true;
+        }
+        if (MAP[2][0] == symbol && MAP[2][1] == symbol && MAP[2][2] == symbol) {
+            return true;
+        }
+        if (MAP[0][0] == symbol && MAP[1][0] == symbol && MAP[2][0] == symbol) {
+            return true;
+        }
+        if (MAP[0][1] == symbol && MAP[1][1] == symbol && MAP[2][1] == symbol) {
+            return true;
+        }
+        if (MAP[0][2] == symbol && MAP[1][2] == symbol && MAP[2][2] == symbol) {
+            return true;
+        }
+        if (MAP[0][0] == symbol && MAP[1][1] == symbol && MAP[2][2] == symbol) {
+            return true;
+        }
+        if (MAP[2][0] == symbol && MAP[1][1] == symbol && MAP[0][2] == symbol) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private static boolean checkDraw() {
+        /*for (char[] chars : MAP) {
+            for (char symbol : chars) {
+if(symbol == DOT_EMPTY){
+    return false;
+}
+            }
+        }
+        return false;*/
+return turnsCount >= SIZE * SIZE;
+    }
+
+    private static boolean isCellFree(int rowNumber, int columnNumber) {
+        return MAP[rowNumber][columnNumber] == DOT_EMPTY;
+    }
 
     private static void aiTurn() {
+        System.out.println("Ход компьютера!");
 
+        int rowNumber;
+        int columnNumber;
+
+
+        /*} while (true) {
+            rowNumber = random.nextInt(SIZE);
+            columnNumber = random.nextInt(SIZE);
+            if (isCellFree(rowNumber, columnNumber)) {
+                break;
+            }*/
+
+        do {
+            rowNumber = random.nextInt(SIZE);
+            columnNumber = random.nextInt(SIZE);
+        } while (!isCellFree(rowNumber, columnNumber));
+
+        MAP[rowNumber][columnNumber] = DOT_AI;
+        turnsCount++;
     }
 
 }
